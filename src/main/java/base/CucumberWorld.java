67ce62cbe0557e5f3c3c16cf1dd75c8390c07509
Ross <r_cooper85@hotmail.com>
2020-05-05 18:41:36 +0100

    Added a new BUILD_DATE field to the CucumberWorld class used for the Browserstack build capability

--- src/main/java/base/CucumberWorld.java
+++ src/main/java/base/CucumberWorld.java
@@ -13,15 +13,25 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class CucumberWorld {
 
+    Date date = new Date();
+
     //ReadConfig config = new ReadConfig();
     public static WebDriver driver;
+    SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
     public static final String USERNAME = System.getProperty("username");
     private final String AUTOMATE_KEY = System.getProperty("seleniumgridkey");
     private final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+    private final String BUILD_DATE = sdf.format(date);
 
     public CucumberWorld() throws FileNotFoundException {
     }
@@ -37,10 +47,11 @@ public class CucumberWorld {
     public void setupWorld() throws MalformedURLException {
 
        String pombrowser = System.getProperty("browser");
+       //LocalDate date = LocalDate.now();
 
         // IF statement to determine which browser to instantiate
         if ("chrome".equals(pombrowser)) {
-            System.out.println("Loading chromedriver for Chrome");
+            System.out.println("Loading chromedriver for Chrome " + BUILD_DATE);
             System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ross\\Desktop\\Repos\\JavaCucumberTAF\\src\\main\\resources\\chromedriver.exe");
             driver = new ChromeDriver();
             driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -59,7 +70,7 @@ public class CucumberWorld {
             caps.setCapability("os_version", "10");
             caps.setCapability("resolution", "1024x768");
             caps.setCapability("name", "Bstack- Java Framework for Ross");
-            caps.setCapability("build", "Test build for Ross");
+            caps.setCapability("build", "Test build for Ross - " + BUILD_DATE);
             caps.setCapability("project", "Java Cucumber Framework");
             driver = new RemoteWebDriver(new URL(URL), caps);
             driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
